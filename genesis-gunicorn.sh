8384ec54a1d605a1194c6960559f3e4a57d5bd1a
yudanta <me@yudanta.web.id>
2020-12-09 09:36:47 +0700

    set gunicorn.sh for heroku deploy

--- genesis-gunicorn.sh
+++ genesis-gunicorn.sh
@@ -1,2 +1,2 @@
-# gunicorn --name 'Diterawang, yet another image Classification API' --chdir /home/genesis/src --timeout 300 -b 0.0.0.0:$PORT app:app -k gevent --worker-connections 5 --workers 1 --reload
-gunicorn --name 'Diterawang, yet another image Classification API' --chdir /home/genesis/src --timeout 300 -b 0.0.0.0:8000 app:app -k gevent --worker-connections 5 --workers 1 --reload
+gunicorn --name 'Diterawang, yet another image Classification API' --chdir /home/genesis/src --timeout 300 -b 0.0.0.0:$PORT app:app -k gevent --worker-connections 5 --workers 1 --reload
+# gunicorn --name 'Diterawang, yet another image Classification API' --chdir /home/genesis/src --timeout 300 -b 0.0.0.0:8000 app:app -k gevent --worker-connections 5 --workers 1 --reload
